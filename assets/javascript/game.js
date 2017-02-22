e5dd3e9b817b1f9528c856eb57692a62d270f99a
Dereck Jones <dereck.it83@gmail.com>
2017-02-22 14:50:36 -0500

    latest

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//javascript
+
+window.onload = function () {
+	var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+		document.write();
+}
+alphabet();
+
+for (var i = alphabet().length - 1; i >= 0; i--) {
+	alphabet()[i]
+}
